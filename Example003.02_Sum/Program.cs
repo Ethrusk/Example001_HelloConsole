36b78cc69d277ba936c7981baeed40b1ad9dea0c
Ethrusk <ethrusk@yandex.ru>
2023-10-14 21:47:33 +0300

    Example003.02_Sum Random

--- Example003.02_Sum/Program.cs
+++ Example003.02_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Тренировка Example003.02_Sum
 int numberC = new Random ().Next(1,10);
+Console.WriteLine(numberC);
 int numberD = new Random ().Next(1,10);
+Console.WriteLine(numberD);
 int result = numberC+ numberD;
 Console.WriteLine(result);

297954d9f2a7a19d57bbaa52f67ee37638b35ba9
Ethrusk <ethrusk@yandex.ru>
2023-10-15 12:51:38 +0300

    Example005-2.0_If_Else new folder

---
+++ Example005-2.0_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿// Тренировка Example005-2.0_If_Else
+Console.Write("Введите имя пользователя: ");
+string? username = Console.ReadLine()!;
+
+if(username.ToLower()! == "мила")
+{
+    Console.WriteLine("Ура! Это же Мила!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
